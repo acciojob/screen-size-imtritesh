9cab8278364b971da5e6b9ac9018a597c94d3269
imtritesh <103854328+imtritesh@users.noreply.github.com>
2023-05-23 18:03:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //your JS code here. If required.
 let div = document.querySelector("#sizeInfo");
 let h1 = document.createElement("h1");
-div.append(h1);
 function resize(){
-	h1.textContent = `Width:${window.innerWidth} and Height:${window.innerHeight}`;
+	h1.textContent = `Width: ${window.innerWidth} and Height: ${window.innerHeight}`;
+	div.append(h1);
 }
