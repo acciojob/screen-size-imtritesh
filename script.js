d4fd48d4167e06737dab76ad7ed5b693b62ee685
imtritesh <103854328+imtritesh@users.noreply.github.com>
2023-05-23 17:54:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+let body = document.body;
+let h1 = document.createElement("h1");
+body.append(h1);
+function resize(){
+	h1.textContent = `Width:${window.innerWidth} and Height:${window.innerHeight}`;
+}
